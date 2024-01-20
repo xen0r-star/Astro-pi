04b03b73aa5cab6484bdcfb0300fa01a8446ea2f
Florian <florian241006@gmail.com>
2024-01-20 23:37:42 +0100

    Documentation

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# from picamera import PiCamera
+# from time import sleep
+
+# camera = PiCamera()
+# camera.resolution = (2592, 1944)
+
+# for i in range(3):
+#     camera.capture(f'image_{i:03d}.jpg')
