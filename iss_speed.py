6ac3e4542733bc6cf7cb9d35a77da463474f4136
Thibaut <102216285+thibautddrt@users.noreply.github.com>
2024-01-23 20:38:42 +0000

    Create iss_speed.py

---
+++ iss_speed.py
@@ -0,0 +1,13 @@
+from exif import Image
+from datetime import datetime
+
+
+def get_time(image):
+    with open(image, 'rb') as image_file:
+        img = Image(image_file)
+        time_str = img.get("datetime_original")
+        time = datetime.strptime(time_str, '%Y:%m:%d %H:%M:%S')
+    return time
+
+
+print(get_time('photo_0683.jpg'))
