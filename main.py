f304994538ec512d7afaa7ea0407fd89a19685c1
thibautddrt <alphonse19400@gmail.com>
2024-01-23 13:31:13 +0100

    Partie 2.2

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# from picamera import PiCamera
-# from time import sleep
-
-# camera = PiCamera()
-# camera.resolution = (2592, 1944)
-
-# for i in range(3):
-#     camera.capture(f'image_{i:03d}.jpg')
+print("Hello")
